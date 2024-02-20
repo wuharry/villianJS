c977016f30ea775dcf005882ff8b67f28e92f53e
wuharry <whw880218cool@gmail.com>
2024-02-20 09:23:04 +0800

    feat:init

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+
+console.log('init');
+// document.querySelector('#app')
+
+
